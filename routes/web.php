04b706639a745593d15aee75bb7a69d264dc78c4
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-16 00:52:41 -0400

    Add Sanctum in project'

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,8 @@ use App\Http\Controllers\OrderItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->middleware('api')->group(function () {
-
     Route::apiResource('products', ProductController::class);
-    
     Route::apiResource('categories', CategoryController::class);
-
     Route::apiResource('orders', OrderController::class);
-
     Route::apiResource('order-items', OrderItemController::class);
 });
