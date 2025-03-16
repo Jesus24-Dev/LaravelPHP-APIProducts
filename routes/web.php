ed45780b8a2f619fdb19b66f266d4b4f796afcb4
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-16 00:11:11 -0400

    Add api endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('api')->middleware('api')->group(function () {
+
+    Route::apiResource('products', ProductController::class);
+    
+    Route::apiResource('categories', CategoryController::class);
+
+    Route::apiResource('orders', OrderController::class);
+
+    Route::apiResource('order-items', OrderItemController::class);
 });
