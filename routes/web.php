1461d1a03655b1e8f175e8b6bc9cefc2dc2f53ef
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-16 15:27:27 -0400

    remove routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,2 @@
 <?php
 
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderItemController;
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('api')->middleware('api')->group(function () {
-    Route::apiResource('products', ProductController::class);
-    Route::apiResource('categories', CategoryController::class);
-    Route::apiResource('orders', OrderController::class);
-    Route::apiResource('order-items', OrderItemController::class);
-});
