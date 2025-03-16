6e0abdf0a8c9e9accdb688f46c6582a51701f5b1
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-16 16:29:11 -0400

    Created basic authentication with Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,23 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderItemController;
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/register', function(Request $request){
+    $authController = new AuthController();
+    return $authController->register($request);
 });
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('api')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+
     Route::apiResource('products', ProductController::class);
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('orders', OrderController::class);
