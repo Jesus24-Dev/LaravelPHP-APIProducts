ae9561b0fd180772c0c12a01f278c7b9e83fb215
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-15 22:34:46 -0400

    Created tables. Deleted unused default tables

--- database/migrations/2025_03_16_010000_create_categories_table.php
+++ database/migrations/2025_03_16_010000_create_categories_table.php
@@ -6,15 +6,14 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
- 
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->decimal('price', 10, 2)->check('price >= 0');
-            $table->integer('quantity')->check('quantity >= 0');
-            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
@@ -24,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 };
