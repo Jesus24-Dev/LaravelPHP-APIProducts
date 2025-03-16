6c9530bd4c231753b530f2243e785f6427d07be7
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-15 22:34:10 -0400

    Created models for API

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
